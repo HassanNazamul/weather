7b34c4911572312f5f7bb979efa1cdd21b5922a0
Mohammed <hassan.nazamul05@gmail.com>
2024-04-21 16:47:15 -0400

    done

--- src/Api.js
+++ src/Api.js
@@ -1,11 +1,11 @@
 export const geoApiOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "cf7222bc6amsh0f008549ec85bc1p16295bjsn1e95c0fbd495",// enter your rapid api key here
+    "X-RapidAPI-Key": "cf7222bc6amsh0f008549ec85bc1p16295bjsn1e95c0fbd495",
     "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
   },
 };
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-export const WEATHER_API_KEY = "e66c42734b3c641feb993d197d71838c"; // enter your key from openweather API
+export const WEATHER_API_KEY = "e66c42734b3c641feb993d197d71838c";
